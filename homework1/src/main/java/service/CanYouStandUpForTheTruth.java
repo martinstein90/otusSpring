ab8ef177ddbbdade2cc2b25b38a453fce0f75705
martin <martimstein90@yahoo.com>
2018-07-01 22:21:40 +0300

    Все. Плюс еще тесты

--- homework1/src/main/java/service/CanYouStandUpForTheTruth.java
+++ homework1/src/main/java/service/CanYouStandUpForTheTruth.java
@@ -11,7 +11,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 public class CanYouStandUpForTheTruth implements InterviewService{
 
@@ -24,46 +23,57 @@ public class CanYouStandUpForTheTruth implements InterviewService{
 
     @Override
     public void interview(String userName, int amountQuestions) {
-        List<Integer> questionsNumbers = Arrays.asList(dao.getQuestionsNumbers());
-        Collections.shuffle(questionsNumbers);
+        List<Integer> questionsNumbers = getQuestionsShuffleNumbers();
 
         System.out.println("Привет, " + userName + "!");
         for(int i = 0; i < amountQuestions; i++) {
-            System.out.println("Вопрос " + (i+1));
             Question question = dao.findQuestionByNumber(questionsNumbers.get(i));
-            System.out.println(question.getQustion());
-            String[] answers = question.getAnswers();
-            for (int j = 0; j < answers.length; j++) {
-                System.out.println( (j+1) + ") " + answers[j]);
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            int selectedAnswerNumber = -1;
-            while(true) {
-                String line;
-                try {
-                    line = reader.readLine();
-                } catch (IOException e) {
-                    System.out.println("Что то с клавиатурой!");
-                    throw new RuntimeException(e);
-                }
-                try {
-                    selectedAnswerNumber = Integer.parseInt(line);
-                }
-                catch (NumberFormatException e)  {
-                    System.out.println("Не число!");
-                }
+            System.out.println("Вопрос " + (i+1) + " " + question.getQustion());
 
-                if(selectedAnswerNumber > 0 && selectedAnswerNumber <= answers.length)
-                    break;
-                else
-                    System.out.println("Введите число от 1 до " + answers.length);
-            }
-            Integer score = question.getScore(answers[selectedAnswerNumber]);
-            System.out.println(score);
+            String[] answers = question.getAnswers();
+            for (int j = 0; j < answers.length; j++)
+                System.out.println((j+1) + ") " + answers[j]);
+
+            int selectedAnswerNumber = getSelectedAnswerNumber(answers.length);
+
+            Integer score = question.getScore(answers[selectedAnswerNumber-1]);
             scores += score;
         }
 
-        Result result = dao.getResultByScore(scores);
+        Result result = dao.getResultByScore(scores * amountQuestions / questionsNumbers.size());
         System.out.println("Результат: " +result.getResult() );
+        System.out.println(userName + "благодарим за участие!");
+    }
+
+    private List<Integer> getQuestionsShuffleNumbers() {
+        List<Integer> questionsNumbers = Arrays.asList(dao.getQuestionsNumbers());
+        Collections.shuffle(questionsNumbers);
+        return questionsNumbers;
+    }
+
+    private int getSelectedAnswerNumber(int upperLimit) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int selectedAnswerNumber = -1;
+        while(true) {
+            String line;
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                System.out.println("Что то с клавиатурой!");
+                throw new RuntimeException(e);
+            }
+            try {
+                selectedAnswerNumber = Integer.parseInt(line);
+            }
+            catch (NumberFormatException e)  {
+                System.out.println("Не число!");
+            }
+
+            if(selectedAnswerNumber > 0 && selectedAnswerNumber <= upperLimit)
+                break;
+            else
+                System.out.println("Введите число от 1 до " + upperLimit);
+        }
+        return selectedAnswerNumber;
     }
 }
