98e7e305c78f22c4e49a0e35abfc971c327ff2d3
martin <martimstein90@yahoo.com>
2018-07-02 20:56:54 +0300

    1

--- homework1/src/main/java/Main.java
+++ homework1/src/main/java/Main.java
@@ -1,17 +1,10 @@
-import dao.InterviewDao;
-import dao.InterviewFromCSVdao;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.CanYouStandUpForTheTruth;
 import service.InterviewService;
 
 public class Main {
-
     public static void main(String[] args) {
-        //InterviewFromCSVdao("homework1\\src\\main\\resources\\Can you stand up for the truth.csv");
-
-
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        InterviewDao dao = context.getBean(InterviewFromCSVdao.class);
         InterviewService service = context.getBean(CanYouStandUpForTheTruth.class);
         service.interview("Ivan", 5);
     }
